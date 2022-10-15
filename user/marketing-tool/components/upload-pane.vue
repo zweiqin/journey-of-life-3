34fcdcdf42d6a2b52cd3e01c6ddcaf0c4924f63f
solitary-cloud <1946306647@qq.com>
2022-10-15 16:15:13 +0800

    1111

---
+++ user/marketing-tool/components/upload-pane.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="uploade-pane-container">
+    <view class="title">门店logo</view>
+
+    <view class="body">
+      <view class="sub-title">上传图片</view>
+      <view class="main">
+        <van-uploader></van-uploader>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+@import "../../../style/var.less";
+@import "../../../style/mixin.less";
+
+.uploade-pane-container {
+  .title {
+    color: @c3d;
+    font-size: @f12;
+    margin: 48upx 0 20upx 0;
+  }
+
+  .body {
+    border: 1upx solid #d8d8d8;
+    border-radius: @r10;
+    padding: 32upx 16upx;
+    box-sizing: border-box;
+
+    .sub-title {
+      color: @c9;
+      font-size: @f12;
+    }
+  }
+}
+</style>
---
+++ user/marketing-tool/store-application.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="store-application-container">
+    <view class="header">
+      <img src="" class="back" alt="" />
+    </view>
+    <FieldPane
+      :fields="applyStoreOne"
+      title="账号信息"
+      v-model="form.accountInfo"
+    ></FieldPane>
+
+    <FieldPane
+      :fields="applyStoreTow"
+      title="门店信息"
+      v-model="form.storeInfo"
+    ></FieldPane>
+
+    <UploadPane></UploadPane>
+  </view>
+</template>
+
+<script>
+import FieldPane from "./components/field-pane.vue";
+import UploadPane from "./components/upload-pane.vue";
+import { applyStoreOne, applyStoreTow } from "./config";
+
+export default {
+  components: {
+    FieldPane,
+    UploadPane,
+  },
+  data() {
+    return {
+      applyStoreOne,
+      applyStoreTow,
+      form: {
+        accountInfo: {},
+        storeInfo: {},
+      },
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.store-application-container {
+  padding: 40upx;
+  box-sizing: border-box;
+}
+</style>
