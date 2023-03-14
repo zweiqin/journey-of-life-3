67f974c4da86803ec3188d2c66520dc1ccad8f6c
zweiqin <zweiqin88@163.com>
2023-03-14 18:07:01 +0800

    个人中心

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const BASE_URL = 'https://www.zhult.com'
-// export const RUAN_URL = 'https://www.tuanfengkeji.cn:9527/dts-app-api/wx' // 生产
-export const RUAN_URL = 'https://www.tuanfengkeji.cn:8098/dts-app-api/wx' // 测试
+export const RUAN_URL = 'https://www.tuanfengkeji.cn:9527/dts-app-api/wx' // 生产
+// export const RUAN_URL = 'https://www.tuanfengkeji.cn:8098/dts-app-api/wx' // 测试
 // export const RUAN_URL = 'http://192.168.0.108:8080/demo/'
 
 
