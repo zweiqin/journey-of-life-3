e814da0fb22532c1603598ba36922ce666fa7766
GuYun-D <solitary-cloud>
2023-05-06 19:35:52 +0800

    ✨ feat(zlt): 获取团蜂家居页面url

---
+++ forLogistics.js
@@ -0,0 +1,20 @@
+/**
+ * 此处是为了在助力通后台方便获取当前url，设置压屏窗跳转链接
+ */
+const routerMethods = [
+  "navigateTo",
+  "redirectTo",
+  "reLaunch",
+  "switchTab",
+  "navigateBack",
+  "",
+];
+
+for (const method of routerMethods) {
+  uni.addInterceptor(method, {
+    invoke() {
+      window.parent.postMessage(window.location.href, "*");
+      return true;
+    },
+  });
+}
--- main.js
+++ main.js
@@ -1,99 +1,103 @@
-import Vue from 'vue'
-import App from './App'
-import store from './store'
-import globalMixin from './mixin/global'
+import Vue from "vue";
+import App from "./App";
+import store from "./store";
+import globalMixin from "./mixin/global";
 
-Vue.config.productionTip = false
+// #ifdef H5
+import "./forLogistics";
+// #endif
+
+Vue.config.productionTip = false;
 //echar引入
-import * as echarts from 'echarts'
-Vue.prototype.$echarts = echarts
+import * as echarts from "echarts";
+Vue.prototype.$echarts = echarts;
 Vue.prototype.$baseImgUrl =
-  'https://www.tuanfengkeji.cn:9527/dts-admin-api/admin/storage/fetch/'
-Vue.filter('replacestar', function (value) {
-  if (!value) return ''
-  let str = value
-  str = str.replace(new RegExp('[^0-9]+', 'g'), '')
+  "https://www.tuanfengkeji.cn:9527/dts-admin-api/admin/storage/fetch/";
+Vue.filter("replacestar", function (value) {
+  if (!value) return "";
+  let str = value;
+  str = str.replace(new RegExp("[^0-9]+", "g"), "");
   if (str.length == 11) {
-    str = str.toString().replace(/(\d{3})\d*(\d{4})/, '$1****$2')
+    str = str.toString().replace(/(\d{3})\d*(\d{4})/, "$1****$2");
   } else {
-    str = str.toString().replace(/(\d{3})\d*(\d{4})/, '$1****$2')
+    str = str.toString().replace(/(\d{3})\d*(\d{4})/, "$1****$2");
   }
-  return str
-})
+  return str;
+});
 
 Vue.mixin({
   methods: {
     setData: function (obj) {
-      let that = this
-      let keys = []
-      let val, data
+      let that = this;
+      let keys = [];
+      let val, data;
       Object.keys(obj).forEach(function (key) {
-        keys = key.split('.')
-        val = obj[key]
-        data = that.$data
+        keys = key.split(".");
+        val = obj[key];
+        data = that.$data;
         keys.forEach(function (key2, index) {
           if (index + 1 == keys.length) {
-            that.$set(data, key2, val)
+            that.$set(data, key2, val);
           } else {
             if (!data[key2]) {
-              that.$set(data, key2, {})
+              that.$set(data, key2, {});
             }
           }
-          data = data[key2]
-        })
-      })
+          data = data[key2];
+        });
+      });
     },
     $showToast(text, icon) {
       uni.showToast({
         title: text,
         duration: 2000,
-        icon: icon || 'none',
-      })
+        icon: icon || "none",
+      });
     },
     go(url) {
-      if (!url) return
+      if (!url) return;
       uni.navigateTo({
         url,
-      })
+      });
     },
     getSize(selecter) {
-      const _this = this
+      const _this = this;
       return new Promise((resolve, reject) => {
-        const query = uni.createSelectorQuery().in(_this)
+        const query = uni.createSelectorQuery().in(_this);
         query
           .select(selecter)
-          .boundingClientRect(data => {
-            resolve(data)
+          .boundingClientRect((data) => {
+            resolve(data);
           })
-          .exec()
-      })
+          .exec();
+      });
     },
 
     empty() {
       uni.showToast({
-        title: '功能升级中...',
-        icon: 'none',
-      })
+        title: "功能升级中...",
+        icon: "none",
+      });
     },
 
     isLogin() {
-      return !!this.$store.getters.userId
+      return !!this.$store.getters.userId;
     },
   },
-})
+});
 
-Vue.use(globalMixin)
+Vue.use(globalMixin);
 
-App.mpType = 'app'
+App.mpType = "app";
 
 const app = new Vue({
   ...App,
   store,
-})
-app.$mount()
+});
+app.$mount();
 
-import filters from './utils/filter' //导入过滤器文件
-Object.keys(filters).forEach(key => {
+import filters from "./utils/filter"; //导入过滤器文件
+Object.keys(filters).forEach((key) => {
   //通过Object.key方法取出过滤器中导出的每个方法并挂在vue.filter上
-  Vue.filter(key, filters[key])
-})
+  Vue.filter(key, filters[key]);
+});
