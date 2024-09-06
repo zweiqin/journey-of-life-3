5260b81b7327487929e5bb5df2b394b4263feff3
zweiqin <zweiqin88@163.com>
2024-09-06 19:01:31 +0800

    2024.09.06 补充提交

--- config/index.js
+++ config/index.js
@@ -1,36 +1,36 @@
-// // 线上助力通
-// export const SheQu1_URL = 'https://www.zhult.com/' // 线上
-// // export const SheQu1_URL = "http://192.168.0.144:8781/"; // 本地
-// // 线上团蜂
-// export const SheQu_URL = 'https://www.tuanfengkeji.cn/dts-app-api' // 线上
-// export const RUAN_URL = 'https://www.tuanfengkeji.cn/dts-app-api/wx' // 线上
-// export const BASE_WS_API = 'wss://www.tuanfengkeji.cn:8098/dts-app-api/ws' // 线上
-// // export const BASE_WS_API = 'wss://www.tuanfengkeji.cn:8080/api/ws'; // 线上
-// // export const BASE_WS_API = 'wss://www.tuanfengkeji.cn:8098/dts-admin-api/webSocket'  // 测试
-// // 师傅的
-// export const sf = 'https://www.zhult.com/samrtWorker' // 线上
-// // 团蜂H5项目地址
-// export const A_TF_MAIN = 'https://www.tuanfengkeji.cn/TFShop_Uni_H5' // 线上
-// // 新团蜂H5的接口路径
-// export const ANOTHER_TF_INTERFACE = 'https://nsappapi.tuanfengkeji.cn' // 线上
-// // export const ANOTHER_TF_INTERFACE = 'http://192.168.0.91:9107' // 本地
-// export const ANOTHER_TF_UPLOAD = 'https://nsappapi.tuanfengkeji.cn/file/upload' // 线上
-// // 新团蜂入驻端项目地址
-// export const ANOTHER_TF_SETTLE = 'https://nsh51.tuanfengkeji.cn' // 线上
-// // 团蜂商家端的接口路径
-// export const MER_TF_INTERFACE = 'https://nsbusinessapi.tuanfengkeji.cn' // 线上
+// 线上助力通
+export const SheQu1_URL = 'https://www.zhult.com/' // 线上
+// export const SheQu1_URL = "http://192.168.0.144:8781/"; // 本地
+// 线上团蜂
+export const SheQu_URL = 'https://www.tuanfengkeji.cn/dts-app-api' // 线上
+export const RUAN_URL = 'https://www.tuanfengkeji.cn/dts-app-api/wx' // 线上
+export const BASE_WS_API = 'wss://www.tuanfengkeji.cn:8098/dts-app-api/ws' // 线上
+// export const BASE_WS_API = 'wss://www.tuanfengkeji.cn:8080/api/ws'; // 线上
+// export const BASE_WS_API = 'wss://www.tuanfengkeji.cn:8098/dts-admin-api/webSocket'  // 测试
+// 师傅的
+export const sf = 'https://www.zhult.com/samrtWorker' // 线上
+// 团蜂H5项目地址
+export const A_TF_MAIN = 'https://www.tuanfengkeji.cn/TFShop_Uni_H5' // 线上
+// 新团蜂H5的接口路径
+export const ANOTHER_TF_INTERFACE = 'https://nsappapi.tuanfengkeji.cn' // 线上
+// export const ANOTHER_TF_INTERFACE = 'http://192.168.0.91:9107' // 本地
+export const ANOTHER_TF_UPLOAD = 'https://nsappapi.tuanfengkeji.cn/file/upload' // 线上
+// 新团蜂入驻端项目地址
+export const ANOTHER_TF_SETTLE = 'https://nsh51.tuanfengkeji.cn' // 线上
+// 团蜂商家端的接口路径
+export const MER_TF_INTERFACE = 'https://nsbusinessapi.tuanfengkeji.cn' // 线上
 
-export const SheQu1_URL = 'https://test.zhult.com/' // 测试
-export const SheQu_URL = 'https://test.tuanfengkeji.cn/dts-app-api' // 测试
-export const RUAN_URL = 'https://test.tuanfengkeji.cn/dts-app-api/wx' // 测试
-export const sf = 'https://test.zhult.com/samrtWorker' // 测试
-export const A_TF_MAIN = 'https://test.tuanfengkeji.cn/TFShop_Uni_H5' // 测试
+// export const SheQu1_URL = 'https://test.zhult.com/' // 测试
+// export const SheQu_URL = 'https://test.tuanfengkeji.cn/dts-app-api' // 测试
+// export const RUAN_URL = 'https://test.tuanfengkeji.cn/dts-app-api/wx' // 测试
+// export const sf = 'https://test.zhult.com/samrtWorker' // 测试
+// export const A_TF_MAIN = 'https://test.tuanfengkeji.cn/TFShop_Uni_H5' // 测试
 // export const ANOTHER_TF_INTERFACE = 'https://nsappapitest.tuanfengkeji.cn' // 测试
-export const ANOTHER_TF_UPLOAD = 'https://nsappapitest.tuanfengkeji.cn/file/upload' // 测试
-export const ANOTHER_TF_SETTLE = 'https://nsh51test.tuanfengkeji.cn' // 测试
-export const MER_TF_INTERFACE = 'https://nsbusinessapitest.tuanfengkeji.cn' // 测试
+// export const ANOTHER_TF_UPLOAD = 'https://nsappapitest.tuanfengkeji.cn/file/upload' // 测试
+// export const ANOTHER_TF_SETTLE = 'https://nsh51test.tuanfengkeji.cn' // 测试
+// export const MER_TF_INTERFACE = 'https://nsbusinessapitest.tuanfengkeji.cn' // 测试
 
-export const ANOTHER_TF_INTERFACE = 'http://192.168.0.91:9107' // 本地
+// export const ANOTHER_TF_INTERFACE = 'http://192.168.0.91:9107' // 本地
 // export const IMG_UPLOAD_URL = 'https://www.tuanfengkeji.cn:9527/dts-app-api/wx/storage/upload' // 线上，旧团蜂文件上传接口
 // export const IMG_UPLOAD_URL = 'https://test.tuanfengkeji.cn/dts-app-api/wx/storage/upload' // 测试，旧团蜂文件上传接口
 
